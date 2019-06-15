3b18bb5a524ceca08421af6a709c37569c41a3bf
cOOkIE <whdrnr01@gmail.com>
2019-06-15 16:55:40 +0900

    Calculator using ES6

--- calculator/app.js
+++ calculator/app.js
@@ -1,1 +1,22 @@
-console.log('app.js');
+const btns = document.querySelectorAll('.btn');
+const screen = document.querySelector('.screen');
+const equalBtn = document.querySelector('.btn-equal');
+const clearBtn = document.querySelector('.btn-clear');
+
+btns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        screen.textContent += btn.getAttribute('data-num');
+    });
+});
+
+equalBtn.addEventListener('click', () => {
+    if (screen.textContent === "") {
+        alert('입력값이 없습니다.');
+        return false;
+    }
+    screen.textContent = eval(screen.textContent);
+});
+
+clearBtn.addEventListener('click', () => {
+    screen.textContent = "";
+});
