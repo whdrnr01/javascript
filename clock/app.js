6f5a8d2dab9bce62c30c4718b73c91ed828950e0
cOOkIE <whdrnr01@gmail.com>
2019-06-18 16:49:53 +0900

    Clock

--- clock/app.js
+++ clock/app.js
@@ -1,7 +1,18 @@
 const clock = document.querySelector('.clock');
 
-setInterval(() => {
+function showTime() {
     let d = new Date();
-    let ampm = (d.getHours() > 12) ? "PM" : "AM";
-    clock.innerHTML = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds() + " " + ampm;
-}, 1000);
+    let ampm = (d.getHours() < 12 || d.getHours() === 24) ? "AM" : "PM";
+    let hours = addZero((d.getHours() >= 12) ? d.getHours() - 12 : d.getHours());
+    clock.innerHTML = hours + ":" + addZero(d.getMinutes()) + ":" + addZero(d.getSeconds()) + " " + ampm;
+}
+
+function addZero(time) {
+    if (time < 10) {
+        time = '0' + time;
+    }
+    return time;
+}
+
+showTime();
+setInterval(showTime, 1000);
